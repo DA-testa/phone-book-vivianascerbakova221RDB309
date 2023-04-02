a1ee53f7f5b13d0ad9328238425cec8391fa2b0e
Viviana Scerbakova <125687697+vivianascerbakova221RDB309@users.noreply.github.com>
2023-04-03 01:05:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,25 +20,15 @@ def process_queries(queries):
     contacts = []
     for cur_query in queries:
         if cur_query.type == 'add':
+            contacts[cur_query.number] = cur.query.name
+        elif cur_query.type == 'del':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            if cur_query.number in contacts:
+                del contacts[cur_query.number]
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+
+            response = contacts.get(cur_query.number, 'not found')
             result.append(response)
     return result
 
